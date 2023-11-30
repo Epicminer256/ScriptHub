3c4f1b72242b83b89f5bdd79d2332a05125dae77
Epicminer256 <nathanross2024@gmail.com>
2023-11-29 18:58:55 -0600

    Fixed indents

--- ScriptHub.ps1
+++ ScriptHub.ps1
@@ -2,74 +2,74 @@
 $local:Version = "Prerelease v0.0.0"
 
 # API Variables
-$local:SH_Path = $PSScriptRoot
+$local:SH_Path = $PSScriptRoot # Note, this may not have ScriptHub in it
 
-$updateError = 0
+function private:Start-ScriptHub{
 
+    #Set the location
+    Set-Location $SH_Path
 
-Function private:Start-ScriptHub{
+    #cls
 
-#Set the location
-Set-Location $SH_Path
-
-#cls
-
-if ($error -eq '1')
-{
-Write-Host 'Error, Not A Valid Response.' -fore Red
-}
-
-Write-Host "
- #####                               #     #               
-#     #  ####  #####  # #####  ##### #     # #    # #####  
-#       #    # #    # # #    #   #   #     # #    # #    # 
- #####  #      #    # # #    #   #   ####### #    # #####  
-      # #      #####  # #####    #   #     # #    # #    # 
-#     # #    # #   #  # #        #   #     # #    # #    # 
- #####   ####  #    # # #        #   #     #  ####  #####
-"
-
-#Has to wait or color won't be green - No idea why !!Only in ISE!!
-Start-Sleep -mill 10
-
-Write-Host "Version: $($version)" -Fore Green
-Write-Host "Created by HelpMeGame with help from SCR33M `n" -fore Green
-
-echo 'Where Would You Like To Go? (Type The Number Of Where You Would Like To Go To)
-1) Background
-2) Dark Mode
-3) Update Log (Non-functional)
-4) Shortcuts
-5) Update (Non-functional)
-'
-
-$private:Select = Read-Host
-
-if ($Select -eq '1')
-{
-    & .\Scripts\Background.ps1
-} elseif($Select -eq '2')
+    if ($error -eq '1')
     {
-    & .\Scripts\Darkmode.ps1
-    } elseif($Select -eq '3')
-        {
+        Write-Host 'Error, Not A Valid Response.' -fore Red
+    }
+
+    Write-Host "
+    #####                               #     #               
+    #     #  ####  #####  # #####  ##### #     # #    # #####  
+    #       #    # #    # # #    #   #   #     # #    # #    # 
+    #####  #      #    # # #    #   #   ####### #    # #####  
+        # #      #####  # #####    #   #     # #    # #    # 
+    #     # #    # #   #  # #        #   #     # #    # #    # 
+    #####   ####  #    # # #        #   #     #  ####  #####
+    "
+
+    #Has to wait or color won't be green - No idea why !!Only in ISE!!
+    Start-Sleep -mill 10
+
+    Write-Host "Version: $($version)" -Fore Green
+    Write-Host "Created by HelpMeGame with help from SCR33M `n" -fore Green
+
+    echo 'Where Would You Like To Go? (Type The Number Of Where You Would Like To Go To)
+    1) Background
+    2) Dark Mode
+    3) Update Log (Non-functional)
+    4) Shortcuts
+    5) Update (Non-functional)
+    '
+
+    $private:Select = Read-Host
+
+    if ($Select -eq '1')
+    {
+        & .\Scripts\Background.ps1
+    }
+    if($Select -eq '2')
+    {
+        & .\Scripts\Darkmode.ps1
+    }
+    if($Select -eq '3')
+    {
         & .\UpdateLog.txt
         Start-ScriptHub
-        } elseif($Select -eq '4')
-            {
-            & .\Scripts\Shortcuts.ps1
-            } elseif($Select -eq '5')
-                {
-                & .\Scripts\Update\Update.ps1 $($version)
-                } elseif ($Select -eq 'Incognito')
-                    {
-                    & .\Scripts\Incognito.ps1
-                    } else
-                        {
-                        cls
-                        $error = 1
-                        Start-ScriptHub
-                        }
+    }
+    if($Select -eq '4')
+    {
+        & .\Scripts\Shortcuts.ps1
+    }
+    if($Select -eq '5')
+    {
+        & .\Scripts\Update\Update.ps1 $($version)
+    }
+    if ($Select -eq 'Incognito')
+    {
+        & .\Scripts\Incognito.ps1
+    }
+    cls
+    $error = 1
+    Start-ScriptHub
 }
 
 cls
