aa3325b1758b556e8a608813172c2fc41e19cda6
Epicminer256 <nathanross2024@gmail.com>
2023-11-29 18:19:59 -0600

    Changed folder structure

---
+++ ScriptHub.ps1
@@ -0,0 +1,82 @@
+# When forking or updating, change these settings
+$Version = "Prerelease v0.0.0"
+
+
+$updateError = 0
+
+function Get-ScriptDirectory {
+    Split-Path -parent $PSCommandPath
+}
+
+#cls
+
+Function Start-ScriptHub{
+
+#Get the script location
+$localLocation = Get-ScriptDirectory
+
+#Set the location
+Set-Location $localLocation
+
+#cls
+
+if ($error -eq '1')
+{
+Write-Host 'Error, Not A Valid Response.' -fore Red
+}
+
+Write-Host "
+ #####                               #     #               
+#     #  ####  #####  # #####  ##### #     # #    # #####  
+#       #    # #    # # #    #   #   #     # #    # #    # 
+ #####  #      #    # # #    #   #   ####### #    # #####  
+      # #      #####  # #####    #   #     # #    # #    # 
+#     # #    # #   #  # #        #   #     # #    # #    # 
+ #####   ####  #    # # #        #   #     #  ####  #####
+"
+
+#Has to wait or color won't be green - No idea why !!Only in ISE!!
+Start-Sleep -mill 10
+
+Write-Host "Version: $($version)" -Fore Green
+Write-Host "Created by HelpMeGame with help from SCR33M `n" -fore Green
+
+echo 'Where Would You Like To Go? (Type The Number Of Where You Would Like To Go To)
+1) Background
+2) Dark Mode
+3) Update Log (Non-functional)
+4) Shortcuts
+5) Update (Non-functional)
+'
+
+$Select = Read-Host
+
+if ($Select -eq '1')
+{
+    & .\Scripts\Background.ps1
+} elseif($Select -eq '2')
+    {
+    & .\Scripts\Darkmode.ps1
+    } elseif($Select -eq '3')
+        {
+        & .\UpdateLog.txt
+        Start-ScriptHub
+        } elseif($Select -eq '4')
+            {
+            & .\Scripts\Shortcuts.ps1
+            } elseif($Select -eq '5')
+                {
+                & .\Scripts\Update.ps1 $($version)
+                } elseif ($Select -eq 'Incognito')
+                    {
+                    & .\Scripts\Incognito.ps1
+                    } else
+                        {
+                        cls
+                        $error = 1
+                        Start-ScriptHub
+                        }
+}
+
+cls
+Start-ScriptHub
---
+++ Scripts/Update/Update.ps1
@@ -0,0 +1,136 @@
+#----------------------------------------------------#
+#    _____           _       _   _    _       _      #
+#   / ____|         (_)     | | | |  | |     | |     #
+#  | (___   ___ _ __ _ _ __ | |_| |__| |_   _| |__   #
+#   \___ \ / __| '__| | '_ \| __|  __  | | | | '_ \  #
+#   ____) | (__| |  | | |_) | |_| |  | | |_| | |_) | #
+#  |_____/ \___|_|  |_| .__/ \__|_|  |_|\__,_|_.__/  #
+#                     | |                            #
+#                     |_|                            #
+#----------------------------------------------------#
+
+#Get the script directory
+$CurrentDir = $PSScriptRoot
+
+#Set the directory
+Set-Location $CurrentDir
+
+#Go up in directory
+$MainDir = Split-Path -Path $CurrentDir -Parent
+
+#Set verison
+$Version = $args[0]
+
+
+function CheckForUpdate {
+    cls
+
+    #Sets script dir
+    Set-Location $CurrentDir
+
+    #Set the uri for the latest release
+    $URI = "https://api.github.com/repos/Epicminer256/ScriptHub/releases/latest"
+
+    #Gets latest release tag
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $ReleaseTag = (Invoke-WebRequest -Uri $URI -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+    
+    if ($ReleaseTag -gt $Version) {
+        Write-Host "New version available! Would you like to update?"
+        $YN = Read-Host �[Y] Yes [N] No�
+        if ($YN -eq 'y' -or $YN -eq 'Y') {
+            Update
+        } elseif ($YN -eq 'n' -or $YN -eq 'N') {
+            & .\Update.ps1
+        } else {
+            & .\Update.ps1
+        }
+    } else
+    {
+        Write-Host "Latest Update [$($ReleaseTag)] already installed!"
+        read-host �Press ENTER to continue...�
+        & .\Update.ps1
+    }
+}
+
+function Update {
+    cls
+    write-host "Downloading..."
+    #Query the API to get the url of the zip
+    $Response = Invoke-RestMethod -Method Get -Uri $URI
+    $ZipUrl = $Response.zipball_url
+
+    #Download the file to the current location
+    $OutputPath = "$((Get-Location).Path)\$($Response.name.Replace(" ","_")).zip"
+    invoke-RestMethod -Method Get -Uri $ZipUrl -OutFile $OutputPath
+
+    $temp = "C:\users\$env:username\AppData\local\temp"
+
+    #Copy update to temp
+    Copy-Item -path $CurrentDir\ScriptHub_$ReleaseTag.zip -destination $temp
+    Remove-Item -path $CurrentDir\ScriptHub_$ReleaseTag.zip
+
+    
+    cls 
+    write-host "Extracting..."
+
+    #extract
+    expand-archive -Path $temp\ScriptHub_$ReleaseTag.zip -destination $temp\ScriptHub_Temp
+    Remove-Item -path $temp\ScriptHub_$ReleaseTag.zip
+
+    cls
+    write-host "Installing..."
+
+    $filename = Get-ChildItem -Path $temp\ScriptHub_Temp\
+    $StartDir = Split-Path -Path $MainDir -Parent
+
+    #copy over
+    Remove-Item -Path $StartDir/* -Force
+    Copy-Item -Recurse -force "$temp\ScriptHub_Temp\$filename\*" -Destination $CurrentDir
+    Remove-Item -path $temp\ScriptHub_Temp
+    
+}
+
+cls
+
+Write-Host "
+#     #                                   
+#     # #####  #####    ##   ##### ###### 
+#     # #    # #    #  #  #    #   #      
+#     # #    # #    # #    #   #   #####  
+#     # #####  #    # ######   #   #      
+#     # #      #    # #    #   #   #      
+ #####  #      #####  #    #   #   ###### 
+"
+#Has to wait or color won't be green - No idea why !!Only in ISE!!
+Start-Sleep -mill 10
+
+Write-Host "Developed by SCR33M, Improved By HelpMeGame " -fore Green
+Write-Host "Version: $($version)" -fore Green
+
+echo '
+Type the number of the action you would like to do.
+1) Check for and install updates
+2) Revert Versions -- Currently Under Construction
+3) Return To ScriptHub
+'
+
+#read numbers typed
+$Select = Read-Host
+
+#Elif hell
+if ($Select -eq '1')
+    {
+        CheckForUpdate
+    } elseif ($Select -eq '2') 
+        {
+            & .\Update.ps1
+        } elseif ($Select -eq '3')
+            {
+            &..\ScriptHub.ps1
+            } elseif ($Select -ne 1 -and 2 -and 3)
+                {
+                & .\Update.ps1
+                }
+
+
