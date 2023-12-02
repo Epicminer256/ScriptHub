10cac5cb3a7378e8670ca6a74394b76fc3662aac
Epicminer256 <nathanross2024@gmail.com>
2023-12-01 23:47:02 -0600

    Dark mode :)

---
+++ src/darkmode.ps1
@@ -0,0 +1,52 @@
+$mode = 0
+function ask
+{
+    SHPrint "Created By HelpMeGame and improved by EpicMiner256"
+    SHPrint "Light or Dark mode?"
+    $input = SHInput "light/dark/exit"
+    $input = $input.trim()
+    if($input -eq "light")
+    {
+        $mode = 1
+        runit
+        SHPrint "Light Mode Enabled`n"
+        return 0
+    }
+    if($input -eq "dark")
+    {
+        $mode = 0
+        runit
+        SHPrint "Dark Mode Enabled`n"
+        return 0
+    }
+    if($input -eq "exit")
+    {
+        return 1
+        return
+    }
+    SHPrint "Option not valid"
+    return 0
+}
+
+
+
+function runit
+{
+    $OrigLocation = Get-Location
+    Set-Location -Path 'HKLM:\Software\Microsoft'
+    New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value "${mode}" -PropertyType dword -Force
+    New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUseLightTheme' -Value "${mode}" -PropertyType dword -Force
+    New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value "${mode}" -PropertyType dword -Force
+    Pop-Location
+
+    Set-Location -Path $OrigLocation
+}
+
+while(1)
+{
+    $returnVal = ask
+    if($returnVal -eq 1)
+    {
+        break
+    }
+}
